fa6c23326ebd418672752b41c21f04ee66ae00a6
Ronald Holshausen <rholshausen@dius.com.au>
2020-09-29 12:03:48 +1000

    chore: bootstrap springboot project

---
+++ provider/src/main/java/io/pactflow/example/sirenprovider/SirenProviderApplication.java
@@ -0,0 +1,13 @@
+package io.pactflow.example.sirenprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenProviderApplication.class, args);
+	}
+
+}
---
+++ provider/src/test/java/io/pactflow/example/sirenprovider/SirenProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pactflow.example.sirenprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
