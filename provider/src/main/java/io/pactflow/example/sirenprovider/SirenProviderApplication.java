64f64ba921589cbdcaeab2bd631e2a298394c7b9
Ronald Holshausen <rholshausen@dius.com.au>
2020-09-29 12:08:57 +1000

    chore: switch to Java 11

--- provider/src/main/java/io/pactflow/example/sirenprovider/SirenProviderApplication.java
+++ provider/src/main/java/io/pactflow/example/sirenprovider/SirenProviderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SirenProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SirenProviderApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SirenProviderApplication.class, args);
+  }
 
 }
